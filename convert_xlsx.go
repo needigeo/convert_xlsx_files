d689b0e6354850afe85b753311f299d114ce2a34
Dmitriy Neyolov <dmitry.neyolov@gmail.com>
2022-05-04 12:07:04 +0300

    v.1.0.0.0

---
+++ convert_xlsx.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/plandem/xlsx"
+)
+
+type fileStruct struct {
+	fullname  string
+	name      string
+	sheetname string
+}
+
+func main() {
+	inputDir := flag.String("inputDir", "null", "Путь к каталогу с исходными файламии, должен заканчиваться слешем")
+	outputDir := flag.String("outputDir", "null", "Путь к каталогу в который будут сохранены файлы после обработки")
+	flag.Parse()
+
+	fileslice := make([]fileStruct, 1)
+
+	if *inputDir == "null" {
+		log.Fatal("Не задан входной каталог")
+	}
+
+	if *outputDir == "null" {
+		log.Fatal("Не задан входной каталог")
+	}
+
+	files, err := ioutil.ReadDir(*inputDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fullname := *inputDir + file.Name()
+
+		xl, err := xlsx.Open(fullname)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for sheets := xl.Sheets(); sheets.HasNext(); {
+			_, sheet := sheets.Next()
+			fileslice = append(fileslice, fileStruct{fullname: fullname, name: file.Name(), sheetname: sheet.Name()})
+			fmt.Println(fullname, "  ", sheet.Name())
+		}
+
+		xl.Close()
+		//fmt.Println(fullname, file.Name(), file.IsDir())
+	}
+
+	for _, num := range fileslice[1:] {
+		dssheets := []int{99999}
+		i := 0
+		xl, err := xlsx.Open(num.fullname)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for sheets := xl.Sheets(); sheets.HasNext(); {
+			_, sheet := sheets.Next()
+			if sheet.Name() != num.sheetname {
+				dssheets = append(dssheets, i)
+			}
+			i = i + 1
+		}
+		for _, index := range dssheets[1:] {
+			fmt.Println(index)
+			xl.DeleteSheet(index)
+		}
+		newfullname := *outputDir + num.sheetname + " " + num.name
+		fmt.Println(newfullname)
+		err = xl.SaveAs(newfullname)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(len(dssheets))
+		xl.Close()
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module gighub.com/needigeo/convert_xlsx_files
+
+go 1.17
+
+require (
+	github.com/plandem/ooxml v1.1.2 // indirect
+	github.com/plandem/xlsx v1.0.4 // indirect
+)
